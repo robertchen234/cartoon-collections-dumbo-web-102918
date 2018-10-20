3e852adb1378b72e7038720ddeab052bf0f2eaa6
Robert Chen <robertchen234@gmail.com>
2018-10-20 22:14:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map { |element| element.capitalize += "!" }
 end
 
 def long_planeteer_calls# code an argument here
