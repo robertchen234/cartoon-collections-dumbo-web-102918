acb675e648ceb53d45833b82a61dd71348dbcbe7
Robert Chen <robertchen234@gmail.com>
2018-10-20 22:20:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find{ |type| cheese == type }
+  cheese_types.find{ |type| cheese_types.include?(cheese) }
 end
