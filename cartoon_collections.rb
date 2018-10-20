2a36406bb3295577531f41611cb4ae653d36390b
Robert Chen <robertchen234@gmail.com>
2018-10-20 22:10:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index{ |dwarf, idx| puts "#{idx+1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)
   # Your code here
 end
 
